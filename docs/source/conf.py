10ede369cccc2f1dbc8dc362ae8ce4c31b60eee2
zzapzzap <42794484+zzapzzap@users.noreply.github.com>
2021-03-12 17:13:27 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,8 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+extensions = ['recommonmark',
+             'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -54,6 +55,7 @@ exclude_patterns = []
 #
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
+html_baseurl = 'https://zzapzzap.github.io/hello/'
 html_favicon = 'Sierra_BASE.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
